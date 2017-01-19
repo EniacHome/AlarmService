b2ed9566e5b604ac84e6be3028ef1fb3b9190105
Lars Gardien <larsgardien@live.nl>
2017-01-19 08:27:55 +0100

    Add JSON convert

--- src/main/java/com/eniacdevelopment/EniacHome/Serial/EventListenerObservers/AndroidListenerObserver.java
+++ src/main/java/com/eniacdevelopment/EniacHome/Serial/EventListenerObservers/AndroidListenerObserver.java
@@ -2,10 +2,12 @@ package com.eniacdevelopment.EniacHome.Serial.EventListenerObservers;
 
 import com.eniacdevelopment.EniacHome.Business.Contracts.SensorService;
 import com.eniacdevelopment.EniacHome.DataModel.Sensor.Sensor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.android.gcm.server.Message;
 import com.google.android.gcm.server.Sender;
 
 import javax.inject.Inject;
+import java.util.Map;
 
 /**
  * Created by larsg on 1/17/2017.
@@ -14,12 +16,13 @@ public class AndroidListenerObserver extends EventListenerObserver  {
 
     private final Sender sender;
     private final SensorService sensorService;
+    private final ObjectMapper objectMapper;
 
     @Inject
-    public AndroidListenerObserver(Sender sender, SensorService sensorService){
-
+    public AndroidListenerObserver(Sender sender, SensorService sensorService, ObjectMapper objectMapper) {
         this.sender = sender;
         this.sensorService = sensorService;
+        this.objectMapper = objectMapper;
     }
 
     @Override
@@ -27,19 +30,13 @@ public class AndroidListenerObserver extends EventListenerObserver  {
         //Get all SensorInfo from the repository
         Sensor sensor = this.sensorService.getSensor(sensorId);
 
-        if (sensor == null) {
-            sensor = new Sensor() {{
-                Id = sensorId;
-                Name = "Unknown";
-                Level = Integer.MAX_VALUE;
-                SensorType = com.eniacdevelopment.EniacHome.DataModel.Sensor.SensorType.Unknown;
-                Enabled = false;
-                SensorStatus = null;
-            }};
-        }
+        Map<String, Object> mapSensor = this.objectMapper.convertValue(sensor, Map.class);
 
-        Message message = new Message.Builder()
-                .priority(sensor.SensorStatus.Alarmed ? Message.Priority.HIGH : Message.Priority.NORMAL)
-                .build();
+        Message.Builder message = new Message.Builder()
+                .priority(sensor.SensorStatus.Alarmed ? Message.Priority.HIGH : Message.Priority.NORMAL);
+
+        for (Map.Entry<String, Object> mapEntry : mapSensor.entrySet()) {
+            message.addData(mapEntry.getKey(), mapEntry.getValue().toString());
+        }
     }
 }
