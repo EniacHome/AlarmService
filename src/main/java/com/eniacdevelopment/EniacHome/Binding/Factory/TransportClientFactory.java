35e8a12b21bbd71d72c91237d4903b0f0ef81e35
Lars Gardien <larsgardien@live.nl>
2017-01-19 20:23:50 +0100

    Added local config

--- src/main/java/com/eniacdevelopment/EniacHome/Binding/Factory/TransportClientFactory.java
+++ src/main/java/com/eniacdevelopment/EniacHome/Binding/Factory/TransportClientFactory.java
@@ -4,18 +4,36 @@ import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.glassfish.hk2.api.Factory;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import java.net.InetSocketAddress;
+import java.util.Properties;
 
 /**
  * Created by larsg on 12/7/2016.
  */
 public class TransportClientFactory implements Factory<TransportClient> {
+    private final static String TRANSPORTCLIENTS = "ELASTIC_CLIENTS";
+    private final Properties properties;
     private TransportClient transportClient;
 
+    @Inject
+    public TransportClientFactory(Properties properties) {
+        this.properties = properties;
+    }
+
     @Override
+    @Singleton
     public TransportClient provide() {
         this.transportClient = TransportClient.builder().build()
                 .addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress("localhost", 9300)));
+
+        String[] transportClients = ((String) this.properties.get(TRANSPORTCLIENTS)).split(";");
+
+        for (String client : transportClients) {
+            String[] values = client.split(":");
+            this.transportClient.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress(values[0], Integer.parseInt(values[1]))));
+        }
 
         return this.transportClient;
     }
